20d527e4496354918451d37fe776d2aafe988b01
Andres <andres.tiko@cern.ch>
2017-01-18 15:37:57 +0200

    Hacks for special cases

--- bin/make_fits.py
+++ bin/make_fits.py
@@ -15,7 +15,7 @@ def mkdir_p(path):
             raise
 
 
-datacard_dir = "output_pseudodata_eleESER2"
+datacard_dir = "output_data_shiftPeak"
 
 
 def read_fit_result(fit_file, postfit_file):
@@ -72,8 +72,11 @@ def combine_cards():
     this_card = "%s/card_%d.txt" % (dc_dir, bin)
     this_ws = "%s/workspace_%d.root" %(ws_dir, bin)
     print "combineCards.py pass=%s/SScards/htt_SS_%d_13TeV.txt fail=%s/cards/OScards/htt_OS_%d_13TeV.txt > %s" % (dc_dir, bin, datacard_dir, bin, this_card)
-    if bin == 6 and "_data" in dc_dir: #Exclude shape systematics when all are zero
-      call("combineCards.py -s pass=%s/SScards/htt_SS_%d_13TeV.txt fail=%s/cards/OScards/htt_OS_%d_13TeV.txt > %s" % (dc_dir, bin, datacard_dir, bin, this_card), shell = True)
+    if bin == 6 and "data" in dc_dir: #SS is totally empty
+      fit_results.append((0,1,1))
+      continue
+      #if bin == 6 and "_data" in dc_dir: #Exclude shape systematics when all are zero
+      #call("combineCards.py -s pass=%s/SScards/htt_SS_%d_13TeV.txt fail=%s/cards/OScards/htt_OS_%d_13TeV.txt > %s" % (dc_dir, bin, datacard_dir, bin, this_card), shell = True)
     else:
       call("combineCards.py pass=%s/SScards/htt_SS_%d_13TeV.txt fail=%s/cards/OScards/htt_OS_%d_13TeV.txt > %s" % (dc_dir, bin, datacard_dir, bin, this_card), shell = True)
     
@@ -83,10 +86,25 @@ def combine_cards():
     call("text2workspace.py %s -o %s -P HiggsAnalysis.CombinedLimit.TagAndProbeModel:tagAndProbe" % (this_card, this_ws), shell = True)
     fit_dir = "./fit_%s/bin%d" % (datacard_dir, bin)
     mkdir_p(fit_dir)
-    call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties" % (this_ws, fit_dir), shell = True)
-    print "combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties" % (this_ws, fit_dir)
+    print "combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1" % (this_ws, fit_dir)
+    #bin 17 increase uncert
+    if ((bin == 1 or bin == 2 or bin==20) and "_data" in fit_dir and "shiftPeak" in fit_dir):
+        call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1 --minimizerStrategy 0" % (this_ws, fit_dir), shell = True)
+    elif ((bin == 4 or bin == 5 or bin == 18) and "_data" in fit_dir and "mva_0_6" in fit_dir):
+        call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1 --minimizerStrategy 0" % (this_ws, fit_dir), shell = True)
+    elif ((bin == 1 or bin == 2 or bin == 7 or bin == 10 or bin == 12 or bin == 13) and "_data" in fit_dir and "mva_0_6" in fit_dir):
+        call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1 --minimizerStrategy 2" % (this_ws, fit_dir), shell = True)
+    elif ((bin == 17 or bin == 20) and "_data" in fit_dir and "mva_0_6" in fit_dir):
+        call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1 --preFitValue 1.2" % (this_ws, fit_dir), shell = True)
+    elif ((bin == 11) and "_data" in fit_dir and "mva_0_6" in fit_dir):
+        call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1 --preFitValue 1.3 --minimizerStrategy 2" % (this_ws, fit_dir), shell = True)
+    elif ((bin == 4 or bin == 9 or bin == 18) and "_data" in fit_dir  and "shiftPeak" in fit_dir):
+        call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1 --minimizerStrategy 2" % (this_ws, fit_dir), shell = True)
+    else:
+        call("combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1" % (this_ws, fit_dir), shell = True)
+    print "combine -v0 -M MaxLikelihoodFit %s --out %s --plots --saveNormalizations --skipBOnlyFit --saveShapes --saveWithUncertainties --robustFit 1" % (this_ws, fit_dir)
     print "PostFitShapesFromWorkspace -d %s -w %s -o %s/output_postfit.root -f %s/mlfit.root:fit_s --postfit --sampling --print" % ("card_%d.txt" % (bin), this_ws, fit_dir, fit_dir)
-    call("PostFitShapesFromWorkspace -d %s -w %s -o %s/output_postfit.root -f %s/mlfit.root:fit_s --postfit --sampling --print" % ("card_%d.txt" % (bin), this_ws, fit_dir, fit_dir), shell=True)
+    call("PostFitShapesFromWorkspace -d %s -w %s -o %s/output_postfit.root -f %s/mlfit.root:fit_s --postfit --sampling" % ("card_%d.txt" % (bin), this_ws, fit_dir, fit_dir), shell=True)
     fit_results.append(read_fit_result("%s/mlfit.root" % fit_dir, "%s/output_postfit.root" % fit_dir))
     #fit_results_mc.append(read_fit_result("%s/mlfit.root" % fit_dir, "%s/output_postfit.root" % fit_dir, isMC = True))
     #print "result", read_fit_result("%s/mlfit.root" % fit_dir, "%s/output_postfit.root" % fit_dir)
@@ -101,4 +119,6 @@ def combine_cards():
     f.write("%d, %.8f, %.8f, %.8f\n" % (i, fr[0], fr[1], fr[2]))
     i += 1
   f.close()
-combine_cards()
+  
+if __name__ == "__main__":
+  combine_cards()
